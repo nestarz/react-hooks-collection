11a9378afc54ef6a86614983b978b81b89859faf
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-02-05 09:50:21 +0100

    uptrack

--- faceapi.js/index.jsx
+++ faceapi.js/index.jsx
@@ -1,79 +1,99 @@
-import { useState, useEffect } from 'react';
 import * as faceapi from 'face-api.js';
+import { useEffect, useState } from 'react';
+import { useInterval } from 'hooks/useInterval';
 
 async function fetchFaces(input) {
-    //console.log("detecting faces...");
-    return await faceapi
-        .detectSingleFace(input, new faceapi.TinyFaceDetectorOptions())
-        .withFaceExpressions()
-    //.withFaceLandmarks();
+  console.log("detecting faces...");
+  return await faceapi
+    .detectSingleFace(input, new faceapi.TinyFaceDetectorOptions())
+    .withFaceExpressions()
+  //.withFaceLandmarks();
 }
 
 async function loadModel(models) {
-    return Promise.all(models.forEach(model => {
-        model.loadModel(model.path)
-    }).map((p) => {
-      p.catch(err => ({ error: err }))
-    }))
+  return Promise.all(models.map((model, i) => {
+    model.netLoader(model.path)
+  }))
 }
 
 export const useFaceApiDetection = (faceApiConfig) => {
-    const [modelReady, setModelReady] = useState(false);
-    const [inputRef, setFaceApiInput] = useState(null);
-    const [results, setResults] = useState(null);
-    const [intervalId, setIntervalId] = useState(null);
-    useEffect(() => {
-        loadModel().then(w => setModelReady(true))
-    }, []);
-    useEffect(() => {
-        console.log("inputRef", inputRef)
-        if (inputRef && modelReady) {
-            clearInterval(intervalId);
-            const id = setInterval(async () => {
-                await fetchFaces(inputRef)
-                    .then(results => setResults(results))
-                    .catch(err => console.log("error", err))
-            }, interval);
-            setIntervalId(id);
-            return () => clearInterval(intervalId);
-        }
-    }, [inputRef, modelReady]);
-    useEffect(() => {
-        if (results && results.detection) {
-            results.detection.lookAt = {
-                x: (results.detection.relativeBox.x + results.detection.relativeBox.width) / 2,
-                y: (results.detection.relativeBox.y + results.detection.relativeBox.height) / 2,
-                z: 0
-            }
-        }
-    }, [results])
-    return [results, setFaceApiInput];
+  const [isActive, setIsActive] = useState(false)
+  const [modelReady, setModelReady] = useState(false);
+  const [results, setResults] = useState(null);
+  const [bboxCenter, setBboxCenter] = useState({
+    x: null,
+    y: null,
+    z: null
+  });
+  const [bestFaceExpression, setBestFaceExpression] = useState({
+    name: null,
+    score: null
+  });
+
+  useInterval(async () => {
+    if (!(modelReady && faceApiConfig.input && isActive)) {
+      console.log("not ok")
+      return
+    }
+    await fetchFaces(faceApiConfig.input)
+      .then(results => setResults(results))
+      .catch(err => console.log("error", err))
+  }, faceApiConfig.interval)
+
+  useEffect(() => {
+    setIsActive(true)
+    loadModel(faceApiConfig.models)
+      .then(w => setModelReady(true))
+    return function cleanup() {
+      setIsActive(false)
+    }
+  }, []);
+
+  useEffect(() => {
+    if (results && results.detection) {
+      setBboxCenter({
+        x: (results.detection.relativeBox.x + results.detection.relativeBox.width) / 2,
+        y: (results.detection.relativeBox.y + results.detection.relativeBox.height) / 2,
+        z: 0
+      })
+    }
+    if (results && results.expressions) {
+      setBestFaceExpression(findBestExpression(results.expressions))
+    }
+  }, [results])
+
+  return { bboxCenter, bestFaceExpression };
 }
 
 function clearCanvas(canvas) {
-    var context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    var w = canvas.width;
-    canvas.width = 1;
-    canvas.width = w;
+  var context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  var w = canvas.width;
+  canvas.width = 1;
+  canvas.width = w;
 }
 
 export const useFaceApiOverlay = (results, canvasRef) => {
-    useEffect(() => {
-        if (results && results.detection && canvasRef.current) {
-            const canvas = canvasRef.current;
-            clearCanvas(canvas)
-            const detectionsForSize = faceapi.resizeResults(results.detection, { width: canvas.width, height: canvas.height })
-            faceapi.drawDetection(canvas, detectionsForSize, { withScore: true })
-        }
-    }, [results, canvasRef.current])
+  useEffect(() => {
+    if (results && results.detection && canvasRef.current) {
+      const canvas = canvasRef.current;
+      clearCanvas(canvas)
+      const detectionsForSize = faceapi.resizeResults(results.detection, { width: canvas.width, height: canvas.height })
+      faceapi.drawDetection(canvas, detectionsForSize, { withScore: true })
+    }
+  }, [results, canvasRef.current])
 }
 
-export const findBestExpression = (expressions) => {
-    if(typeof expressions == "undefined" || 
-        expressions == null || 
-        expressions.length == 0) {
-        return []
-    }
-    
+
+const findBestExpression = (expressions) => {
+  if (typeof expressions == "undefined" ||
+    expressions == null ||
+    expressions.length == 0) {
+    return null
+  }
+  const getBestExpression = (obj) => obj.reduce((obj1, obj2) => {
+    return obj1.probability > obj2.probability ? obj1 : obj2
+  })
+
+  return getBestExpression(expressions)
 }
